85831fd7a9a1697a9458198df3671bb04022e7f6
Stephen Z <mayistep@icloud.com>
2020-08-03 15:38:06 -0400

    ready to deploy

--- app.js
+++ app.js
@@ -3,15 +3,26 @@ const bodyParser = require('body-parser')
 const app = express()
 const cookieParser = require('cookie-parser')
 const indexRouter = require('./routes/index')
+const userRouter = require('./routes/user')
+const path = require('path')
 
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json()) // for parsing application/json
 app.use(cookieParser())
-app.use('/', indexRouter)
+app.use(express.static(path.join(__dirname, 'build')))
+
+
+
 
 app.set('view engine', 'ejs');
 
+app.use('/', indexRouter)
+app.use('/users', userRouter)
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
 
 
 module.exports = app
---
+++ build/precache-manifest.16c718f495f28e1cecfee7f5873c7b78.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b54638650b04f6f2ddbddba64a009d38",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22e70ee57ff23ed01252",
+    "url": "/static/css/2.89441e65.chunk.css"
+  },
+  {
+    "revision": "0c9c2764489013971bb7",
+    "url": "/static/css/main.a5bbf7a4.chunk.css"
+  },
+  {
+    "revision": "22e70ee57ff23ed01252",
+    "url": "/static/js/2.2152043f.chunk.js"
+  },
+  {
+    "revision": "b4397e36447379e6fee78a5a712afec0",
+    "url": "/static/js/2.2152043f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c9c2764489013971bb7",
+    "url": "/static/js/main.4b2d8de4.chunk.js"
+  },
+  {
+    "revision": "4790d0d20ebbde41dd50",
+    "url": "/static/js/runtime-main.d92490f7.js"
+  },
+  {
+    "revision": "7fec41e216a7de828eba1549a5b65f65",
+    "url": "/static/media/avatar1.7fec41e2.png"
+  },
+  {
+    "revision": "79154695cabea0468b6f85923161d4bd",
+    "url": "/static/media/avatar10.79154695.png"
+  },
+  {
+    "revision": "564d80ab875e71c0d42a20338b7875b1",
+    "url": "/static/media/avatar11.564d80ab.png"
+  },
+  {
+    "revision": "fd7a2c9de80cde02bf379f243341a00f",
+    "url": "/static/media/avatar12.fd7a2c9d.png"
+  },
+  {
+    "revision": "7ffd8c725ae06750774bb7ee20f5ed86",
+    "url": "/static/media/avatar13.7ffd8c72.png"
+  },
+  {
+    "revision": "f5481d367f1f568604f9d121ee07f127",
+    "url": "/static/media/avatar14.f5481d36.png"
+  },
+  {
+    "revision": "3bbe276681bb9416b6cab11ae912746f",
+    "url": "/static/media/avatar15.3bbe2766.png"
+  },
+  {
+    "revision": "9ab0a582e40fac198676a08487d412a3",
+    "url": "/static/media/avatar16.9ab0a582.png"
+  },
+  {
+    "revision": "4a9b55fb53c1674682bd488173ce2f37",
+    "url": "/static/media/avatar17.4a9b55fb.png"
+  },
+  {
+    "revision": "a625fc1039d2b30a963853776b22cb41",
+    "url": "/static/media/avatar18.a625fc10.png"
+  },
+  {
+    "revision": "81a807788ee15b877a0f122758aaa618",
+    "url": "/static/media/avatar19.81a80778.png"
+  },
+  {
+    "revision": "3f9137aead829861ce145bd96e6f3abc",
+    "url": "/static/media/avatar2.3f9137ae.png"
+  },
+  {
+    "revision": "1555a8e3eee47f81d445cc51636b9850",
+    "url": "/static/media/avatar20.1555a8e3.png"
+  },
+  {
+    "revision": "ceb9a6479bd734217ea0bae294ba9580",
+    "url": "/static/media/avatar3.ceb9a647.png"
+  },
+  {
+    "revision": "499dcd14e950aa3306d67725c725bcfe",
+    "url": "/static/media/avatar4.499dcd14.png"
+  },
+  {
+    "revision": "caa5a032ebb85a3f2463260aec5501c1",
+    "url": "/static/media/avatar5.caa5a032.png"
+  },
+  {
+    "revision": "7bd50d05530c8700afd3a3b2c815d8a4",
+    "url": "/static/media/avatar6.7bd50d05.png"
+  },
+  {
+    "revision": "03ee16718b4566e5c14d887fc87ad725",
+    "url": "/static/media/avatar7.03ee1671.png"
+  },
+  {
+    "revision": "ac3c12ac6a68c3cc293508b460444bce",
+    "url": "/static/media/avatar8.ac3c12ac.png"
+  },
+  {
+    "revision": "ffd7382329037c28654084bd784e4d69",
+    "url": "/static/media/avatar9.ffd73823.png"
+  }
+]);
