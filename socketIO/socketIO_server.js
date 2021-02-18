c3cbfedb2a5fd4d1727c3bbc67b4c51b1b40080c
Stephen Zheng <65367318+i-le@users.noreply.github.com>
2021-02-17 23:29:10 -0500

    Update socketIO_server.js

--- socketIO/socketIO_server.js
+++ socketIO/socketIO_server.js
@@ -11,7 +11,7 @@ module.exports = function(server) {
       console.log('sever succsfully accepted msg from client side', {from, to, content})
       // storing data (msg) 
       // prep chatMsg obj's data
-      const chat_id = [from, to].sort().join('_') //无论那种方式排序 都只有2个结果，from -> to || to -> from 
+      const chat_id = [from, to].sort().join('_') //无论那种方式排序 都只有2个结果 only two results，from -> to || to -> from 
       const create_time = Date.now()
       new ChatModel({from, to, content, chat_id, create_time}).save(function(err, chatMsg) {
         // sedning msg to client side（A,B..)
